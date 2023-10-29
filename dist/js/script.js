e3a6554fc7590e60c761918b703876d6071f7cc0
Karol Kropisz <karolkropisz@gmail.com>
2023-10-29 16:20:01 +0100

    Add active and remove to image

--- dist/js/script.js
+++ dist/js/script.js
@@ -89,6 +89,7 @@ const select = {
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -170,13 +171,26 @@ const select = {
         price -= option.price;
     }
   }
-        }
+        const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+        const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+        if(optionImage) {
+        if(optionSelected) {
+
+        optionImage.classList.add(classNames.menuProduct.imageVisible)
+
+      }else{
+
+        optionImage.classList.remove(classNames.menuProduct.imageVisible)
+       }  
       }
+     }
+    }
     
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
-
   }
 
   const app = {
