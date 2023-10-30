b56ee6fcd39a40b3b98a5092c0d5bff24344d6ea
Karol Kropisz <karolkropisz@gmail.com>
2023-10-30 21:01:35 +0100

    Add initActions

--- dist/js/script.js
+++ dist/js/script.js
@@ -205,17 +205,19 @@ const select = {
     constructor(element){
       const thisWidget = this;
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('amountWidget', thisWidget);
       console.log('constructor arguments', element);
     }
-  getElements(element){
-    const thisWidget = this;
-
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    getElements(element){
+      const thisWidget = this;
+    
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
   }
 
   setValue(value){
@@ -224,12 +226,30 @@ const select = {
     const newValue = parseInt(value);
 
     /* TODO; Add validation */
-    if(thisWidget.value !== newValue && !isNaN(newValue)){
+    if(thisWidget.value !== newValue && !isNaN(newValue)) {
       thisWidget.value = newValue;
     }
 
     thisWidget.value = newValue;
     thisWidget.input.value = thisWidget.value;
+  }
+
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.input.value);  
+    });
+
+    thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
   }
   }
 
