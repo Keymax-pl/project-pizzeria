0426aaff86fcb10165604a6b717ed2c8901634ca
Karol Kropisz <karolkropisz@gmail.com>
2023-11-06 10:39:57 +0100

    Add API and Ajax

--- dist/js/script.js
+++ dist/js/script.js
@@ -393,6 +393,9 @@
       thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
       thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
       thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+      thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
+      thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
 
     }
 
@@ -411,6 +414,11 @@
       thisCart.dom.productList.addEventListener('remove', function(event ){
         thisCart.remove(event.detail.cartProduct);
       });
+
+      thisCart.dom.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisCart.sendOrder();
+      }); 
     }
 
     add(menuProduct){
@@ -462,6 +470,36 @@
         CartProduct.dom.wrapper.remove();
         thisCart.update();
 
+      }
+
+      sendOrder(){
+        const thisCart = this;
+
+        const url = settings.db.url + '/' + settings.db.orders;
+
+        const payload = {
+          address: thisCart.dom.address.value,
+          phone: thisCart.dom.phone.value,
+          totalPrice: thisCart.totalPrice,
+          subtotalPrice: thisCart.subtotalPrice,
+          totalNumber: thisCart.totalNumber,
+          deliveryFee: settings.cart.defaultDeliveryFee,
+          products: [],
+        };
+
+        for(let prod of thisCart.products){
+          payload.products.push(prod.getData());
+        }
+
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        };
+        
+        fetch(url, options);
       }
 
   }
@@ -532,6 +570,20 @@
         event.preventDefault();
         thisCartProduct.remove();
       });
+    }
+
+    getData(){
+      const thisCartProduct = this;
+
+      const orderSummary = {
+        id: thisCartProduct.id,
+        amount: thisCartProduct.amount,
+        price: thisCartProduct.price,
+        priceSingle: thisCartProduct.priceSingle,
+        name: thisCartProduct.name,
+        params: thisCartProduct.params,
+      };
+      return orderSummary;
     }
   }
 
